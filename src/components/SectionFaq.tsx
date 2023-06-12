025e879615638133f330892a3c640a8556b2e857
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-06-12 20:40:15 -0300

    Create MediumScreen for SectionFAQ

--- src/components/SectionFaq.tsx
+++ src/components/SectionFaq.tsx
@@ -3,9 +3,9 @@ import { Accordeon } from './microComponents/Accordeon'
 
 export function SectionFaq() {
   return (
-    <section className="flex flex-col py-10 px-6 bg-neutral-900 gap-14">
+    <section className="flex flex-col py-10 px-6 md:px-20 bg-neutral-900 gap-14">
       {/* div textos  */}
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5 md:w-[340px]">
         <span className="font-medium text-sm text-gray-500 leading-4">FAQ</span>
 
         <h2 className="font-medium text-[32px] leading-10 text-slate-50">
