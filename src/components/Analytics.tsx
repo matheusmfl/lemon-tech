edac37435bdaf1315b1bcb98b95503ea80a64f2a
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-06-13 18:16:07 -0300

    Add Manual Analytics Script

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script'
+
 export function Analitycs() {
   const GTM_ID = 'GTM-WJ8B4JL'
   return (
@@ -9,6 +11,23 @@ export function Analitycs() {
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','${GTM_ID}');`,
+        }}
+      />
+
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-QYNZTSYH7Y"
+      ></script>
+      <Script
+        strategy="afterInteractive"
+        id="Google-analytics"
+        dangerouslySetInnerHTML={{
+          __html: `
+         window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-QYNZTSYH7Y');`,
         }}
       />
     </>
