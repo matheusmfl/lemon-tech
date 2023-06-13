8c664ad84c7ba3d3cb1cae1d8bbcdadae59ccdd1
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-06-13 18:58:40 -0300

    Add Analytics to layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,6 +3,7 @@ import '../globals.css'
 import { Roboto } from 'next/font/google'
 
 import Head from 'next/head'
+import Script from 'next/script'
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -17,20 +18,23 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <Head>
-        {/* Adicione aqui o código do Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WJ8B4JL')
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-QYNZTSYH7Y"
+      ></Script>
+      <Script
+        id="Analytics"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-QYNZTSYH7Y');
           `,
-          }}
-        />
-      </Head>
+        }}
+      />
+
       <body className={`${roboto.className} overflow-x-hidden`}>
         {/* <!-- Google Tag Manager (noscript) --> */}
         <noscript>
