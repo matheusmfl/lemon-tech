10e8d16e414ada4705d4fcc5368872c599bd3bbd
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-06-14 17:04:09 -0300

    Separator Fix

--- src/components/Section1.tsx
+++ src/components/Section1.tsx
@@ -15,7 +15,7 @@ export function Section1() {
           alt="Separador de página"
           width={2400}
           height={1400}
-          className=" md:hidden"
+          className=" md:hidden object-fill"
         />
         <Image
           src={mdSeparator}
